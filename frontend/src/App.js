eca0f2b51095028f0ed11b2f339acd3e047c2510
Boltzmann <dev.adnan@hotmail.com>
2023-02-19 15:12:26 +0400

    Header, Footer & Bootstrap Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import './index.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <h1>Welcome To ProShop</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+/* 
+    <body>
+        <root>
+            <app>
+                <main>
+                .
+                .
+                .
+            </app>
+        </root>
+    </body>
+*/
+
+export default App
