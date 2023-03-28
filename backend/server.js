6fcef09ef85931ce14c5e7f5f98fc7d0d5a6e25f
Boltzmann <dev.adnan@hotmail.com>
2023-03-28 05:55:51 +0400

    express serves index.html

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const products = require('./data/products')
+const path = require('path')
 
 const app = express()
+const dirname = path.dirname(__dirname)
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+// app.get('/', (req, res) => {
+//     res.send('API is running')
+// })
 
 app.get('/api/products', (req, res) => {
     res.json(products)
 })
 
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
+    const product = products.find((p) => p._id === req.params.id)
     res.json(product)
+})
+
+app.use(express.static(path.join(dirname, '/frontend/build')))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html'))
 })
 
 app.listen(5000, console.log(`Server running on port 5000`))
